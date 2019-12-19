508c642802aeec1e9c18f0f24a47165112b8ec5b
Kiran Johns <kiranjohns369@gmail.com>
2019-12-19 23:38:10 +0530

    qrng setup

---
+++ qrng/qrng.py
@@ -0,0 +1,43 @@
+from qiskit import IBMQ, Aer, QuantumRegister, ClassicalRegister, QuantumCircuit, execute
+from qiskit.providers.ibmq import least_busy
+
+circuit = None
+provider = IBMQ.load_account()
+
+def makeCircuit(n):
+    global circuit
+    qr = QuantumRegister(n)
+    cr = ClassicalRegister(n)
+    circuit = QuantumCircuit(qr,cr)
+    circuit.h(qr)
+    circuit.measure(qr,cr)
+
+
+def setBackend(device='qasm_simulator'):
+    global backend
+    if device=="ibmq_16_melbourne":
+        backend = provider.get_backend(device)
+        makeCircuit(14)
+    if device == "ibmq_qasm_simulator":
+        backend = provider.get_backend(device)
+        makeCircuit(32)
+    if device == 'ibmq_ourense':
+        backend = provider.get_backend(device)
+        makeCircuit(5)
+    if device == 'ibmq_essex':
+        backend = provider.get_backend(device)
+        makeCircuit(5)
+    if device == 'ibmq_vigo':
+        backend = provider.get_backend(device)
+        makeCircuit(5)
+    else:
+        backend = Aer.get_backend('qasm_simulator')
+        makeCircuit(8)
+        
+def getValue():
+    job = execute(circuit,backend=backend,shots=1)
+    result = job.result().get_counts()
+    value = [k for k,v in result.items() if v == 1][0]
+    return value
+
+
